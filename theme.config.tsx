711b0da13be98fcfec8a9867035ea56be1ba1815
Luke Secomb <luke.secomb@live.com>
2023-04-15 16:11:06 +1000

    📝 writing a heap more documentation / adjust nextra config (favicon/seo thumbnail etc.)

--- theme.config.tsx
+++ theme.config.tsx
@@ -10,6 +10,34 @@ const config: DocsThemeConfig = {
   footer: {
     text: 'fflib.dev',
   },
+  head: (
+    <>
+      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+      <link rel='icon' type='image/svg+xml' href='/favicon.svg' />
+      <link rel='icon' type='image/x-icon' href='/favicon.ico' />
+      <meta property='og:title' content='fflib | Apex Enterprise Framework' />
+      <meta
+        property='og:description'
+        content='Unofficial and semi-opinionated fflib documentation. Streamline and standardise your Salesforce development process with fflib'
+      />
+      <meta name='twitter:card' content='summary_large_image' />
+      <meta property='og:image' content='https://fflib.dev/thumbnail.jpg' />
+      <meta name='twitter:image' content='https://fflib.dev/thumbnail.jpg' />
+      <meta property='og:image:alt' content='Logo for LWC Dependency Viewer' />
+      <meta property='og:image:width' content='1200' />
+      <meta property='og:image:height' content='620' />
+      <script
+        data-goatcounter='https://fflib-dev.goatcounter.com/count'
+        async
+        src='//gc.zgo.at/count.js'
+      ></script>
+    </>
+  ),
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s - fflib | Apex Enterprise Framework',
+    }
+  },
 }
 
 export default config
